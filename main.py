16ad64839b2ca521d90a0f87f015ea1a405ff474
Hslven <55009460+Hslven@users.noreply.github.com>
2022-10-11 17:27:29 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def tip():
 
 # 推送信息
 def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips,
-                 note_en, note_ch, health_tip, lucky_,):
+                 note_en, note_ch, health_tip, lucky_, ):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -226,15 +226,18 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
-
-    year = localtime().tm_year
-    future = datetime.strptime('%d-11-08 00:00:00' % year, '%Y-%m-%d %H:%M:%S')
+    mon = 11
+    yea = year
+    # 如果过了今年，跳到下一年
+    if month > mon:
+        yea += 1
+    future = datetime.strptime('%d-%d-08 00:00:00' % (yea, mon), '%Y-%m-%d %H:%M:%S')
     # 当前时间
     now = datetime.now()
     # 时间差
     delta = future - now
     love_both = delta.days
-
+    print(love_both)
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
@@ -324,18 +327,18 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
-    response = post(url, headers=headers, json=data).json()
-    if response["errcode"] == 40037:
-        print("推送消息失败，请检查模板id是否正确")
-    elif response["errcode"] == 40036:
-        print("推送消息失败，请检查模板id是否为空")
-    elif response["errcode"] == 40003:
-        print("推送消息失败，请检查微信号是否正确")
-    elif response["errcode"] == 0:
-        # return 0
-        print("推送消息成功")
-    else:
-        print(response)
+    # response = post(url, headers=headers, json=data).json()
+    # if response["errcode"] == 40037:
+    #     print("推送消息失败，请检查模板id是否正确")
+    # elif response["errcode"] == 40036:
+    #     print("推送消息失败，请检查模板id是否为空")
+    # elif response["errcode"] == 40003:
+    #     print("推送消息失败，请检查微信号是否正确")
+    # elif response["errcode"] == 0:
+    #     # return 0
+    #     print("推送消息成功")
+    # else:
+    #     print(response)
 
 
 if __name__ == "__main__":
